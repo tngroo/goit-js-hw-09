9d2dd8a6c7621f3d8e9fcf9b28057ccfc32cb738
Vladyslav <vsemencenko15@gmail.com>
2026-07-12 16:21:53 +0300

    ready

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const formData = {
+    email: "",
+    message: ""
+}
+
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", event => {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+        localStorage.removeItem(STORAGE_KEY);
+        formData.email = "";
+        formData.message = "";
+        form.reset();
+    }
+});
